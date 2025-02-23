3f9f8186287f5f2391604e3903cc2a07a9c877c4
Erlantz <git@erl.gr>
2025-02-23 19:38:24 +0100

    ariketa erdi inda?

--- ke/4lab.s
+++ ke/4lab.s
@@ -3,7 +3,7 @@ n: .word -5
 main:
   push {r11, lr}
   mov r11, sp
-  push {r0, r1}
+  push {r0, r1} @erabiltzen diren erregistroak
   ldr r0, n
   ldr r0, [n]
   adr r1, n
@@ -11,23 +11,22 @@ main:
   bge ena_pos
   bl neg
 buk:
-  @programaren amaiera
-  pop{r0, r1, r11, pc}
+  pop{r0, r1, r11, pc} @programaren amaiera
 
 ena_pos:
   bl pos
   b buk
 
 pos:
-  @ aaaa
   push{r11, pc}
   mov r11, sp
-  @...
+  add r0, r0, #1
   pop{lr}
 
   
 neg:
-  @ bbbbb
   push{r11, lr}
+  mov r11, sp
+  sub r1, r1, #1
   pop{lr}
 
